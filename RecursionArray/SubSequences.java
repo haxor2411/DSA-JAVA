b4a6dc367d7ec3186136527f693f575dfe37d1e9
haxor2411 <pravalgupta2002@gmail.com>
2024-04-08 15:10:30 +0530

    new file:   RecursionArray/SubSequences.java

---
+++ RecursionArray/SubSequences.java
@@ -0,0 +1,15 @@
+package RecursionArray;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SubSequences {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        ArrayList<String> list = generateSubsequences(str);
+    }
+    public static ArrayList<String> generateSubsequences(String str){
+        
+    }
+}
