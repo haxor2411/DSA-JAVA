1b8e84268e696157c32dfea88e921065f690469c
haxor2411 <pravalgupta2002@gmail.com>
2024-04-08 19:05:27 +0530

    modified:   RecursionArray/SubSequences.java

--- RecursionArray/SubSequences.java
+++ RecursionArray/SubSequences.java
@@ -8,8 +8,24 @@ public class SubSequences {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         ArrayList<String> list = generateSubsequences(str);
+        System.out.println(list);
     }
     public static ArrayList<String> generateSubsequences(String str){
-        
+        if(str == ""){
+            ArrayList<String> res = new ArrayList<>();
+            res.add("");
+            return res;
+        }
+        char ch = str.charAt(0);
+        String roq = str.substring(1);
+        ArrayList<String> subSeq = generateSubsequences(roq);
+        ArrayList<String> mySubSeq = new ArrayList<>();
+        for(String s : subSeq){
+            mySubSeq.add(s);
+        }
+        for(String s : subSeq){
+            mySubSeq.add(ch + s);
+        }
+        return mySubSeq;
     }
 }
