e9ffcb7515a39c070b642dd6712ab28b44908cbe
haxor2411 <pravalgupta2002@gmail.com>
2024-04-23 11:21:27 +0530

    new file:   AlgoComputerNetworks/bitStuffing.java

---
+++ AlgoComputerNetworks/bitStuffing.java
@@ -0,0 +1,27 @@
+package AlgoComputerNetworks;
+public class bitStuffing {
+
+    public static String BitStuffing(String data) {
+        StringBuilder stuffedData = new StringBuilder();
+        int count = 0;
+
+        for (char bit : data.toCharArray()) {
+            if (bit == '1') {
+                count++;
+            } else {
+                count = 0;
+            }
+            stuffedData.append(bit);
+            if (count == 5) {
+                stuffedData.append('0');
+                count = 0;
+            }
+        }
+
+        return stuffedData.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BitStuffing("1111111"));
+    }
+}
