547ed72f9660d460f0f4b20a54384a285e4357d2
haxor2411 <pravalgupta2002@gmail.com>
2024-04-23 22:37:41 +0530

    new file:   AutoDesk/BinaryStringOpr.java

---
+++ AutoDesk/BinaryStringOpr.java
@@ -0,0 +1,55 @@
+package AutoDesk;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BinaryStringOpr{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binaryString = sc.nextLine();
+        int n = sc.nextInt();
+        String[] requests = new String[n];
+        for(int idx = 0; idx < n; idx++){
+            requests[idx] = sc.next();
+        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(String str : requests){
+            if(str.contains(":")){
+                String[] func = str.split(":");
+                arr.add(countFunc(binaryString, Integer.parseInt(func[1])));
+            }
+            else{
+                binaryString = flipFunc(binaryString);
+            }
+        }
+        System.out.println(arr);
+        // System.out.println(countFunc(binaryString, 5));
+        // System.out.println(flipFunc(binaryString));
+    }
+
+    public static int countFunc(String binaryString, int indx){
+        //ArrayList<Integer> freq = new ArrayList<>();
+        int count = 0;
+        if(indx >=0 && indx < binaryString.length()){
+            for(int idx = 0; idx < indx + 1; idx++){
+                if(binaryString.charAt(idx) == '1'){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static String flipFunc(String binaryString){
+        char[] arr = binaryString.toCharArray();
+        for(int idx = 0; idx < arr.length; idx++){
+            if(arr[idx] == '1'){
+                arr[idx] = '0';
+            }
+            else{
+                arr[idx] = '1';
+            }
+        }
+        return new String(arr);
+    }    
+}
