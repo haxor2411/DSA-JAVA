4644a78fd15d9fbba0505bb790ba9a6a9636481b
haxor2411 <pravalgupta2002@gmail.com>
2024-04-26 18:34:52 +0530

    modified:   RecursionLVL2/ObjectChooses/WordPermutation.java

--- RecursionLVL2/ObjectChooses/WordPermutation.java
+++ RecursionLVL2/ObjectChooses/WordPermutation.java
@@ -8,15 +8,27 @@ public class WordPermutation {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        HashMap<Character, Integer> freqMap = new HashMap<>();
+        char[] spots = new char[str.length()];
+        HashMap<Character, Integer> lastOccurence = new HashMap<>();
         for(char ch : str.toCharArray()){
-            if(freqMap.containsKey(ch)){
-                freqMap.put(ch, freqMap.get(ch) + 1);
-            }
-            else{
-                freqMap.put(ch, 1);
+            lastOccurence.put(ch, -1);
+        }
+        generateWords(0, str, spots, lastOccurence);
+    }
+    public static void generateWords(int currentChar, String str, char[] spots, HashMap<Character, Integer> lastOccurence){
+        if(currentChar == str.length()){
+            System.out.println(new String(spots));
+            return;
+        }
+        char currentCharacter = str.charAt(currentChar);
+        for(int idx = lastOccurence.get(currentCharacter) + 1; idx < spots.length; idx++){
+            if(spots[idx] == '\u0000'){
+                spots[idx] = currentCharacter;
+                lastOccurence.put(currentCharacter, idx);
+                generateWords(currentChar + 1, str, spots, lastOccurence);
+                spots[idx] = '\u0000';
+                lastOccurence.put(str.charAt(idx), -1);
             }
         }
-        
     }
 }
