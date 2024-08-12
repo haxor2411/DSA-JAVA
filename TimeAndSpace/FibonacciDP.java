9c6cbe5ce1ae8f4ad5df3cf0ce9f6eda4c60ec1f
haxor2411 <pravalgupta2002@gmail.com>
2024-08-12 14:09:58 +0530

    modified:   TimeAndSpace/FibonacciDP.java

--- TimeAndSpace/FibonacciDP.java
+++ TimeAndSpace/FibonacciDP.java
@@ -1,17 +1,31 @@
 package TimeAndSpace;
 
+// import java.util.Arrays;
+
 public class FibonacciDP {
     public static void main(String[] args) {
-        int n = 5;
-        int[] dp = new int[n + 1];
-        for(int i = 0; i <= n; i++){
-            System.out.print(fibonacci(i, dp) + " ");
-        }
+        int n = 6;
+        // int[] dp = new int[n + 1];
+        // fibonacciM(n, dp);
+        // System.out.println(Arrays.toString(dp));
+        System.out.println(fibonacciT(n));
     }
 
-    static int fibonacci(int n, int[] dp){
+    //Memorisation
+    static int fibonacciM(int n, int[] dp){
         if(n == 0 || n == 1) return dp[n] = n;
         if(dp[n] != 0) return dp[n];
-        return dp[n] = fibonacci(n - 1, dp) + fibonacci(n - 2, dp);
+        return dp[n] = fibonacciM(n - 1, dp) + fibonacciM(n - 2, dp);
+    }
+
+    //Tabulation
+    static int fibonacciT(int n){
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] += dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
