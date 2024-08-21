10866eec9271dc6f7884eb1ecb40a74b65ef0683
haxor2411 <pravalgupta2002@gmail.com>
2024-08-21 21:58:59 +0530

    new file:   DynamicProgramming/Tiling1.java

---
+++ DynamicProgramming/Tiling1.java
@@ -0,0 +1,32 @@
+package DynamicProgramming;
+
+public class Tiling1 {
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println(nWays(n));
+        System.out.println(nWays2(n));
+    }
+
+    static int nWays(int n){
+        int[] dp = new int[n + 1];
+         
+        dp[1] = 1; // for 2 X 1 board you can only put one tile vertically -> 1 Way
+        dp[2] = 2; // for 2 X 2 board you can either put 2 tiles vertically or horizontally -> 2 Way
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    static int nWays2(int n){
+        int f = 1, s = 2;
+        for(int i = 3; i <= n; i++){
+            int t = f + s;
+            f = s;
+            s = t;
+        }
+        return s;
+    }
+}
