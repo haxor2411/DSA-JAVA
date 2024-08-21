d741dc549b80a1a3dffb0c9fbff1ed2e021fd819
haxor2411 <pravalgupta2002@gmail.com>
2024-08-21 22:25:40 +0530

    modified:   DynamicProgramming/TilingMX1.java

--- DynamicProgramming/TilingMX1.java
+++ DynamicProgramming/TilingMX1.java
@@ -1,7 +1,17 @@
 package DynamicProgramming;
-
 public class TilingMX1 {
     public static void main(String[] args) {
-        
+        int n = 39, m = 16;
+        System.out.println(nWays(n, m));
+    }
+
+    static int nWays(int n, int m){
+        int[] dp = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            if(i < m) dp[i] = 1;
+            else if(i == m) dp[i] = 2;
+            else dp[i] = dp[i - 1] + dp [i - m];
+        }
+        return dp[n];
     }
 }
