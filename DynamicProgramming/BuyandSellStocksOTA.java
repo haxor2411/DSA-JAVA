db98fdf295edc2ce039fa8c25a1980802c191e40
haxor2411 <pravalgupta2002@gmail.com>
2024-08-22 14:50:07 +0530

    new file:   DynamicProgramming/BuyandSellStocksOTA.java

---
+++ DynamicProgramming/BuyandSellStocksOTA.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+public class BuyandSellStocksOTA {
+    public static void main(String[] args) {
+        int days = 9;
+        int price[] = {11,6,7,19,4,1,6,18,4};
+        System.out.println(maxProfit(days, price));
+    }
+
+    static int maxProfit(int n, int[] arr){
+        int maxProfit = 0, minPrice = arr[0];
+
+        for(int i = 1; i < n; i++){
+            minPrice = minPrice > arr[i] ? arr[i] : minPrice;
+            maxProfit = maxProfit < arr[i] - minPrice ? arr[i] - minPrice: maxProfit;
+        }
+        return maxProfit;
+    }
+}
