ebe337bc7e2e5480704b91ba12a128b779828c58
haxor2411 <pravalgupta2002@gmail.com>
2024-09-01 22:48:23 +0530

    new file:   Queue/QueueIntro.java

---
+++ Queue/QueueIntro.java
@@ -0,0 +1,29 @@
+package Queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueIntro {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        System.out.println(queue);
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+        
+        System.out.println(queue.remove());
+
+        System.out.println(queue.peek());
+
+        System.out.println(queue);
+
+        System.out.println(queue.size());
+    }
+}
